dca2c59f5d4be2e219443c88c646b2ccb06f2376
qausainmanzoor piaic q2 <qm.reborn@yahoo.com>
2020-11-04 00:11:27 +0500

    Modified app.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deployed React Using <code>Github Actions By </code> Qausian Manzoor 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Qausain"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub Profile
         </a>
       </header>
     </div>
